4bc8b88aadf3da158bdac122c064e1e6c6de467e
Poroto Kekana <archibaldkekana@yahoo.com>
2022-04-28 23:49:34 +0200

    Added higher order function

--- function.js
+++ function.js
@@ -1,1 +1,11 @@
-Hello higher order functions ES6
+//Hello higher order functions ES6
+shapes = [{type: "circle" ,color: "meroon", size: "small"} , {type: "square", color: "red", size:"medium"}]
+
+function mediumShape (shapes) {
+const results = shapes.filter(shape =>{
+    if (shape.size != small) {
+        return shape.type;
+    }
+})
+return results;
+}
